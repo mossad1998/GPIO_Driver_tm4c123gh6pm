9ad5cb587fdf5024bbca62a13bb06841451b33dd
Mosad <50258006+mossad1998@users.noreply.github.com>
2021-08-07 17:52:46 +0200

    Add files via upload

--- GPIO_Driver.h
+++ GPIO_Driver.h
@@ -5,8 +5,6 @@
 #include <string.h>
 
 
-
-
 #ifndef __GPIO_Driver_H__
 #define __GPIO_Driver_H__
 
@@ -18,16 +16,13 @@
 #define CLK_PORTE 	(0x10)
 #define CLK_PORTF 	(0x20)
 
-
-
 extern void CLK_Enable(char port_name);                                         //Clock for Port
-extern void GPIO_PORT_UNLOCK(char port_name);                                   //Unlock for port
-extern void PIN_CONTROL_UNLOCK(char port_name,int pin);                         //Make Changes To Pins
+extern void GPIO_PORT_LOCK(char port_name);                                     //Unlock for port
 extern void PIN_ANALOG_DIGITAL(char port_name,int pin,char type[10]);           //Enable or Disable Analog and Digital
 extern void PIN_FUNCTION_SELECT(char port_name,int pin,char type[15]);          //General i/o or Alternative Function
 extern void PIN_INPUT_OUTPUT(char port_name,int pin,char type[10]);             //Input or Output
 extern void PIN_PULLUP_RESISTOR(char port_name,int pin,char type[10]);          //Pullup or Pulldown
-extern void PIN_CONFIGURE(char port_name,int pin,char type[10]);                //Pullup or Pulldown
+extern void PIN_CONFIGURE(char port_name,int pin,char type[10]);                //Specify the configuration
 extern void PIN_WRITE_DIGITAL(char port_name,int pin,char data[3]);             //To Write Data
 extern bool PIN_READ_DIGITAL(char port_name,int pin);                           //To Read Data
 
